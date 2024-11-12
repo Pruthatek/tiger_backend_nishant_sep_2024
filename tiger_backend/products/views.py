505392f98a19831a74942e5e12f8d62ae39b712c
nishant590 <nishant@pruthatek.com>
2024-11-12 14:06:30 +0530

    updated views

--- tiger_backend/products/views.py
+++ tiger_backend/products/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import CategoryMasterSerializer, SubcategoryMasterSerializer
 from django.db import transaction
-from .models import (CategoryMaster, SubcategoryMaster, BrandMaster, LinkMaster,
+from .models import (CategoryMaster, SubcategoryMaster, BrandMaster, LinkMaster, ShopVariantMapping,
                 ProductMaster, ProductVariant, VariantAttributeMapping, AttributeMaster)
 import os
 from django.http import JsonResponse
@@ -15,7 +15,17 @@ from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework.decorators import permission_classes, api_view
 from tiger_backend.permissions import IsAdminOrSeller
 import traceback
+import posixpath
+import uuid
+import json
+import time
+import ast
 
+def generate_short_unique_filename(extension):
+    # Shortened UUID (6 characters) + Unix timestamp for uniqueness
+    unique_id = uuid.uuid4().hex[:6]  # Get the first 6 characters of UUID
+    timestamp = str(int(time.time()))  # Unix timestamp as a string
+    return f"{unique_id}_{timestamp}{extension}"
 
 # Get all categories
 class CategoryListView(generics.ListAPIView):
@@ -75,9 +85,11 @@ def brand_create(request):
 
                 # If logo file is provided, save it to the static folder
                 if logo_file:
+                    extension = os.path.splitext(logo_file.name)[1]  # Get the file extension
+                    short_unique_filename = generate_short_unique_filename(extension)
                     fs = FileSystemStorage(location=os.path.join(settings.STATIC_ROOT, 'brand_logos'))
-                    logo_path = fs.save(logo_file.name, logo_file)
-                    logo_url = os.path.join('static/brand_logos', logo_path)
+                    logo_path = fs.save(short_unique_filename, logo_file)
+                    logo_url = posixpath.join('static/brand_logos', logo_path)
                 else:
                     logo_url = data.get('logo')  # If no file is provided, use the URL provided in the request
                 
@@ -126,9 +138,11 @@ def brand_update(request, brand_id):
             logo_file = request.FILES.get('logo')  # Get the logo file from the request
 
             if logo_file:
+                extension = os.path.splitext(logo_file.name)[1]  # Get the file extension
+                short_unique_filename = generate_short_unique_filename(extension)
                 fs = FileSystemStorage(location=os.path.join(settings.STATIC_ROOT, 'brand_logos'))
-                logo_path = fs.save(logo_file.name, logo_file)
-                logo_url = os.path.join('static/brand_logos', logo_path)
+                logo_path = fs.save(short_unique_filename, logo_file)
+                logo_url = posixpath.join('static/brand_logos', logo_path)
             else:
                 logo_url = data.get('logo', brand.logo)  # Use existing logo if not updated
 
@@ -182,21 +196,25 @@ def brand_delete(request, brand_id):
             return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
 
 
-@csrf_exempt
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def product_register(request):
     if request.method == 'POST':
         try:
+            if not str(request.user.role) in ['Admin', 'Seller']:
+                return JsonResponse({'status': 'error', 'message': 'You do not have permission to access this.'}, status=403)
             # Begin a transaction
             with transaction.atomic():
                 # Get data from request (you can use request.data if using DRF or request.POST for standard Django form submissions)
                 data = request.POST
 
                 # Extract necessary fields for product and variant creation
+                user_name = request.user.username
                 product_name = data.get('product_name')
                 product_description = data.get('product_description')
                 category_id = data.get('category_id')
                 subcategory_id = data.get('subcategory_id')
-                created_by = data.get('created_by')
+                created_by = user_name 
 
                 # Create the ProductMaster object
                 product = ProductMaster.objects.create(
@@ -204,7 +222,7 @@ def product_register(request):
                     product_description=product_description,
                     category_id=category_id,
                     subcategory_id=subcategory_id,
-                    created_by=created_by,
+                    created_by=user_name,
                 )
 
                 # Create the ProductVariant
@@ -217,32 +235,57 @@ def product_register(request):
                     variant_name=variant_name,
                     variant_description=variant_description,
                     price=price,
-                    created_by=created_by,
+                    created_by=user_name,
                 )
 
                 # Create VariantAttributeMapping entries
-                attributes = data.getlist('attributes')  # assuming it's passed as a list
-                for attribute in attributes:
-                    attribute_name, attribute_value = attribute.split(":")  # Assuming format 'name:value'
-                    variant_attr_mapping = VariantAttributeMapping.objects.create(
+                attributes = data.get('attributes', {})
+                if isinstance(attributes, str):  # Convert to dictionary if received as a JSON string
+                    attributes = json.loads(attributes)
+                for attribute_name, attribute_value in attributes.items():
+                    VariantAttributeMapping.objects.create(
                         product_variant=variant,
                         attribute_name=attribute_name,
                         attribute_value=attribute_value,
-                        created_by=created_by,
+                        created_by=created_by
+                    )
+
+                shop_id = data.get('shop_id')
+                quantity_available = data.get('quantity_available', 0)
+                discounted_price = data.get('discounted_price', None)
+
+                if shop_id:
+                    ShopVariantMapping.objects.create(
+                        shop_id=shop_id,
+                        variant=variant,
+                        price=price,
+                        discounted_price=discounted_price,
+                        quantity_available=quantity_available,
+                        created_by=user_name,
                     )
 
                 # Handle images (for now storing in static folder)
-                image_urls = data.getlist('image_urls')  # A list of image URLs
+                image_urls = request.FILES.getlist('image_urls')  # Use request.FILES for file uploads
                 image_paths = []
-                for image_url in image_urls:
-                    # Save the image locally (just an example, you'd have to handle image fetching)
-                    image_path = os.path.join(settings.STATIC_ROOT, 'product_images', os.path.basename(image_url))
-                    image_paths.append(image_path)
 
-                    # Create LinkMaster entry
+                fs = FileSystemStorage(location=os.path.join(settings.STATIC_ROOT, 'product_images'))  # Define the storage location
+                
+                for image_file in image_urls:
+                    # Generate a unique filename
+                    extension = os.path.splitext(image_file.name)[1]  # Get the file extension
+                    short_unique_filename = generate_short_unique_filename(extension)
+                    
+                    # Save the image file to the filesystem
+                    saved_image = fs.save(short_unique_filename, image_file)
+                    img_url = posixpath.join('static/product_images', saved_image)
+
+                    # Add the image URL to the list of image paths
+                    image_paths.append(img_url)
+
+                    # Create a LinkMaster entry for the image
                     LinkMaster.objects.create(
                         product_variant=variant,
-                        image_url=image_path,  # Save the local path here
+                        image_url=img_url,
                         is_active=True,
                     )
 
